a13ea30b476f40f9cb7a7212c253d52d431de81f
NateMG <56982988+peterwg0@users.noreply.github.com>
2022-02-02 19:00:12 -0800

    Add resetsSubsystems command

---
+++ NRGRobot2022/src/main/java/frc/robot/commands/ResetSubsystems.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SwerveDrive;
+
+public class ResetSubsystems extends CommandBase {
+  private final SwerveDrive swerveDrive;
+
+  /** Creates a new ResetSubsystems. */
+  public ResetSubsystems(SwerveDrive swerveDrive) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.swerveDrive = swerveDrive;
+    addRequirements(swerveDrive);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    swerveDrive.reset();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
