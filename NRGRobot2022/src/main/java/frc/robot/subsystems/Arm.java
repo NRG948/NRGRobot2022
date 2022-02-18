95f97fd6dcc19d5f207502f80cf9e6d309d6b6ca
Ed Reed <reed.ej@hotmail.com>
2022-02-17 19:30:30 -0800

    Add preferences for Arm feedforward constants

--- NRGRobot2022/src/main/java/frc/robot/subsystems/Arm.java
+++ NRGRobot2022/src/main/java/frc/robot/subsystems/Arm.java
@@ -20,7 +20,7 @@ import frc.robot.preferences.RobotPreferencesValue;
 import frc.robot.preferences.RobotPreferences.BooleanValue;
 import frc.robot.preferences.RobotPreferences.DoubleValue;
 
-@RobotPreferencesLayout(groupName = "Arm", column = 3, row = 0, width = 1, height = 3)
+@RobotPreferencesLayout(groupName = "Arm", column = 3, row = 0, width = 2, height = 3, type = "Grid Layout")
 public class Arm extends ProfiledPIDSubsystem {
     @RobotPreferencesValue
     public static final DoubleValue levelAngleOffset = new DoubleValue("Arm", "levelAngleOffset", 85.88);
@@ -30,14 +30,23 @@ public class Arm extends ProfiledPIDSubsystem {
     public static final DoubleValue restingAngle = new DoubleValue("Arm", "restingAngle", -20);
     @RobotPreferencesValue
     public static final BooleanValue enableTab = new BooleanValue("Arm", "enableTab", false);
+    @RobotPreferencesValue
+    public static final DoubleValue kP = new DoubleValue("Arm", "kP", 1.0);
+    @RobotPreferencesValue
+    public static final DoubleValue kS = new DoubleValue("Arm", "kS", 1.0);
+    @RobotPreferencesValue
+    public static final DoubleValue kG = new DoubleValue("Arm", "kG", 1.0);
+    @RobotPreferencesValue
+    public static final DoubleValue kV = new DoubleValue("Arm", "kV", 0.5);
+    @RobotPreferencesValue
+    public static final DoubleValue kA = new DoubleValue("Arm", "kA", 0.1);
 
     private final DigitalInput restingPositionLimitSwitch = new DigitalInput(ArmConstants.kRestingPosChannel);
     private final DigitalInput scoringPositionLimitSwitch = new DigitalInput(ArmConstants.kScoringPosChannel);
     private final PWMVictorSPX m_motor = new PWMVictorSPX(ArmConstants.kMotorPort);
     private final DutyCycleEncoder m_encoder = new DutyCycleEncoder(ArmConstants.kEncoderChannel);
     private final ArmFeedforward m_feedforward = new ArmFeedforward(
-            ArmConstants.kSVolts, ArmConstants.kCosVolts,
-            ArmConstants.kVVoltSecondPerRad, ArmConstants.kAVoltSecondSquaredPerRad);
+            kS.getValue(), kG.getValue(), kV.getValue(), kA.getValue());
 
     /** Create a new ArmSubsystem. */
     public Arm() {
