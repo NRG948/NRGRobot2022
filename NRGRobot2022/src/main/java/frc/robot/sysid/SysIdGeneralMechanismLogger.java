adfe17b4b662b8cf78dfd290c8fe7266859f27f9
ReginaLin2005 <74690711+ReginaLin2005@users.noreply.github.com>
2022-02-20 14:46:22 -0800

    Added the SysIdGeneralMechanismLogger

---
+++ NRGRobot2022/src/main/java/frc/robot/sysid/SysIdGeneralMechanismLogger.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.sysid;
+
+/** Add your docs here. */
+public class SysIdGeneralMechanismLogger extends SysIdLogger{
+
+    private double motorVoltage = 0;
+
+    @Override
+    protected boolean isWrongMechanism() {
+        String mechanism = getMechanism();
+        return !mechanism.equals("Arm") && !mechanism.equals("Elevator") && !mechanism.equals("Simple");
+    }
+
+    public void log(double measuredPosition, double measuredVelocity) {
+        addData(getTimestamp());
+        addData(motorVoltage);
+        addData(measuredPosition);
+        addData(measuredVelocity);
+
+        motorVoltage = getMotorVoltage();
+    }
+
+    @Override
+    public void reset() {
+        super.reset();
+        motorVoltage = 0;
+    }    
+    
+
+}
