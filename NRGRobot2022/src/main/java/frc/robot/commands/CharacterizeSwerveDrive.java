6e90d31e2873050424377b03bf9d2576bb38a8d8
Firenibs <natemg4@outlook.com>
2022-02-20 16:05:51 -0800

    Modified Characterize SwerveDrive command to work with SysID logger

--- NRGRobot2022/src/main/java/frc/robot/commands/CharacterizeSwerveDrive.java
+++ NRGRobot2022/src/main/java/frc/robot/commands/CharacterizeSwerveDrive.java
@@ -4,28 +4,18 @@
 
 package frc.robot.commands;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveDrive;
+import frc.robot.sysid.SysIdDrivetrainLogger;
 
 public class CharacterizeSwerveDrive extends CommandBase {
 
   private final SwerveDrive swerveDrive;
 
-  private final NetworkTableEntry autoSpeedEntry =
-          NetworkTableInstance.getDefault().getEntry("/SmartDashboard/SysIdAutoSpeed");
-  private final NetworkTableEntry telemetryEntry =
-          NetworkTableInstance.getDefault().getEntry("/SmartDashboard/SysIdTelemetry");
-
-  private List<Double> telemetryData = new ArrayList<>();
-
-  private double priorAutospeed = 0.0;
+  private final SysIdDrivetrainLogger logger = new SysIdDrivetrainLogger();
 
   /** Creates a new CharacterizeDrivetrainCommand. */
   public CharacterizeSwerveDrive(SwerveDrive swerveDrive) {
@@ -35,47 +25,43 @@ public class CharacterizeSwerveDrive extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    logger.init();
+  }
+
+  @Override
+  public boolean runsWhenDisabled() {
+    return true;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double now = Timer.getFPGATimestamp();
+    ChassisSpeeds chassisSpeed = swerveDrive.getChassisSpeeds();
+
     double position = swerveDrive.getPose2d().getX();
-    double velocity = swerveDrive.getChassisSpeeds().vxMetersPerSecond;
+    double velocity = chassisSpeed.vxMetersPerSecond;
+    double measuredAngle = swerveDrive.getRotation2d().getRadians();
+    double angularRate = chassisSpeed.omegaRadiansPerSecond;
 
     double battery = RobotController.getBatteryVoltage();
-    double motorVoltage = battery * Math.abs(priorAutospeed);
 
-    double autospeed = autoSpeedEntry.getDouble(0.0);
-    priorAutospeed = autospeed;
+    logger.log(position, velocity, measuredAngle, angularRate);
 
-    swerveDrive.drive(autospeed, 0, 0, true, false);
-
-    telemetryData.add(now);
-    telemetryData.add(autospeed * RobotController.getInputVoltage());
-    telemetryData.add(position);
-    telemetryData.add(velocity);
-
+    // TODO: How are we going to set drive the motor based on the voltage given by SysID
+    // swerveDrive.drive(0, 0, 0, true, false);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    StringBuilder b = new StringBuilder();
-    for (int i = 0; i < telemetryData.size(); ++i) {
-        if (i != 0)
-            b.append(", ");
-        b.append(telemetryData.get(i));
-    }
-    telemetryEntry.setString(b.toString());
-
-    swerveDrive.drive(0, 0, 0, true, false);
+    swerveDrive.stopMotors();
+    logger.sendData();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return DriverStation.isDisabled();
   }
 }
