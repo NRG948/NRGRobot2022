ed045777ae91f55d98cd169ef6ddacd2d4fe8c5e
NateMG <56982988+peterwg0@users.noreply.github.com>
2022-03-13 16:47:56 -0700

    made HOOK_1 and HOOK_2 select either end fof the climber arm

--- NRGRobot2022/src/main/java/frc/robot/subsystems/ClimberHooks.java
+++ NRGRobot2022/src/main/java/frc/robot/subsystems/ClimberHooks.java
@@ -20,40 +20,40 @@ public class ClimberHooks extends SubsystemBase {
     private final PWMVictorSPX climberMotor; 
     private final DoubleSolenoid piston1;
     private final DoubleSolenoid piston2;
-    private final DigitalInput limitSwitch1;
-    private final DigitalInput limitSwitch2;
+    private final DigitalInput beamBreak1;
+    private final DigitalInput beamBreak2;
 
     public enum State {
         OPEN, CLOSED;
     }
 
-    public enum PistonSelection {
-        PISTON_1, PISTON_2;
+    public enum HookSelection {
+        HOOK_1, HOOK_2;
     }
 
     /** Creates a new ExampleSubsystem. **/
     public ClimberHooks() {
-        climberMotor = new PWMVictorSPX(0);
+        climberMotor = new PWMVictorSPX(0); // change this to falcon on CANbus
         piston1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
         piston2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 4, 5);
-        limitSwitch1 = new DigitalInput(5);
-        limitSwitch2 = new DigitalInput(6);
+
+        //the beam breaks will read TBD when it engages the bar
+        beamBreak1 = new DigitalInput(5);
+        beamBreak2 = new DigitalInput(6);
     }
 
     @Override
     public void periodic() {
-    // This method will be called once per scheduler run
-
+      // This method will be called once per scheduler run
     }
 
     @Override
     public void simulationPeriodic() {
         // This method will be called once per scheduler run during simulation
-
     }
 
-    public DoubleSolenoid getPiston(PistonSelection p) {
-        if(p.equals(PistonSelection.PISTON_1)) {
+    public DoubleSolenoid getPiston(HookSelection p) {
+        if (p.equals(HookSelection.HOOK_1)) {
             return piston1;
         }
         else {
@@ -61,20 +61,13 @@ public class ClimberHooks extends SubsystemBase {
         }
     }
 
-    public DoubleSolenoid getP1() {
-        return piston1;
-    }
-
-    public DoubleSolenoid getP2() {
-        return piston2;
-    }
-
-    public DigitalInput getLimitSwitch1() {
-        return limitSwitch1;
-    }
-
-    public DigitalInput getLimitSwitch2() {
-        return limitSwitch2;
+    public boolean getLimitSwitch(HookSelection p) {
+        if (p.equals(HookSelection.HOOK_1)) {
+            return beamBreak1.get();
+        }
+        else {
+            return beamBreak2.get();
+        }
     }
 
     public void rotateMotor() {
@@ -95,15 +88,15 @@ public class ClimberHooks extends SubsystemBase {
     }
 
     /** Returns the current state of the acquirer pistons */
-    public State getState(DoubleSolenoid p) {
-        return p.get() == Value.kForward ? State.OPEN : State.CLOSED;
+    public State getState(HookSelection hook) {
+        return getPiston(hook).get() == Value.kForward ? State.OPEN : State.CLOSED;
     }
 
-    public void setState(State state, DoubleSolenoid p) {
-        p.set(state == State.OPEN ? Value.kForward : Value.kReverse);
+    public void setState(State state, HookSelection hook) {
+        getPiston(hook).set(state == State.OPEN ? Value.kForward : Value.kReverse);
     }
 
-    public void toggleState(DoubleSolenoid p) {
-        setState(getState(p) == State.OPEN ? State.CLOSED : State.OPEN, p);
+    public void toggleState(HookSelection hook) {
+        setState(getState(hook) == State.OPEN ? State.CLOSED : State.OPEN, hook);
     }
 }
