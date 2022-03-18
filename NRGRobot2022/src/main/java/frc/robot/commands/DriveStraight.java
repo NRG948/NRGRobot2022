787e03034092c182efa14b27a821a3a2f139909d
NateMG <56982988+peterwg0@users.noreply.github.com>
2022-03-18 14:15:56 -0700

    stop swerve drive at the end of drive straight command

--- NRGRobot2022/src/main/java/frc/robot/commands/DriveStraight.java
+++ NRGRobot2022/src/main/java/frc/robot/commands/DriveStraight.java
@@ -42,6 +42,7 @@ public class DriveStraight extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    swerveDrive.stopMotors();
   }
 
   // Returns true when the command should end.
